3aad29fc1cfdb5b3ee695afaac1573faccc52239
kimjunhee <manuna530@gmail.com>
2025-01-26 12:07:59 +0900

    feat/공통 예외 처리

---
+++ src/main/java/com/fastcampus/boardserver/exception/handler/CustomExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.fastcampus.boardserver.exception.handler;
+
+import com.fastcampus.boardserver.dto.response.CommonResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handlerRuntimeException(RuntimeException e) {
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "RuntimeException", e.getMessage(), e.getMessage());
+        return new ResponseEntity<>(commonResponse, new HttpHeaders(), commonResponse.getStatus());
+    }
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Object> handlerBoardServerException(RuntimeException e) {
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "BoardServerException", e.getMessage(), e.getMessage());
+        return new ResponseEntity<>(commonResponse, new HttpHeaders(), commonResponse.getStatus());
+    }
+
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handlerException(RuntimeException e) {
+        CommonResponse commonResponse = new CommonResponse(HttpStatus.OK, "Exception", e.getMessage(), e.getMessage());
+        return new ResponseEntity<>(commonResponse, new HttpHeaders(), commonResponse.getStatus());
+    }
+
+}
